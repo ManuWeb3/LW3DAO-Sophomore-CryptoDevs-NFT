ff666239f689bac81eaaac90267d19140111d3cd
ManuWeb3 <manu.web3.0@gmail.com>
2023-01-16 21:36:39 -0700

    Spell correct in index.js

--- constants/index.js
+++ constants/index.js
@@ -7,7 +7,7 @@ const NFT_CONTRACT_ADDRESS_SNEH_NO_OPENSEA = "0xe6279A32510D9cdF6E6b1Dd1ce6B0Cd0
 // this address is without suport for 'OpenSea' + above "sneh" metadata url
 
 const NFT_CONTRACT_ADDRESS = "0x295B62c614B6bBf085f0eaC7F20a377483CdB4a8"
-// suport for 'OpenSea' + below my own metadata url (https://lw3dao-sopho-frontend-cryptodev-nft.vercel.app/)
+// support for 'OpenSea' + below my own metadata url (https://lw3dao-sopho-frontend-cryptodev-nft.vercel.app/)
 
 const METADATA_URL = "https://lw3dao-sopho-frontend-cryptodev-nft.vercel.app/api/";
 
